7e7e08eaa5d290b6e247651ab512f2f142d38b85
Andy Robinson <andy.robinson@thoughtworks.com>
2011-12-08 13:05:45 +0000

    Started to refactor main - looks trcky ...

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 examiner = require('./examiner.js');
 enigmaFactory = require('./enigma.js');
-enigma = enigmaFactory.createEnigma(1);
+enigma = enigmaFactory.createEnigma(6);
 
 console.log('\n\nAttempting to connect to MINISCULUS server\n--------------------------------------------\n');
 
-examiner.getQuestion('/14f7ca5f6ff1a5afb9032aa5e533ad95',function(result) {
+examiner.getQuestion('/start',function(result) {
 	console.log("URL: "+ result["reference-url"]);
 	console.log("question: "+ result["question"]);	
 
@@ -13,7 +13,7 @@ examiner.getQuestion('/14f7ca5f6ff1a5afb9032aa5e533ad95',function(result) {
 		
 		examiner.putAnswer('/14f7ca5f6ff1a5afb9032aa5e533ad95','{"answer":"'+ cipher +'"}', function(result) {
 			console.log("URL: "+ result["reference-url"]);
-			console.log("question: "+ result["question"]);	
+			console.log("question: "+ result["question"]);
 			console.log('\n---------------------\nEnd of response\n');
 		});
 	});
