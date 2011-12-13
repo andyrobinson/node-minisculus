4ab134007124c0cccd9649af91563ead8e3ec64f
Andy Robinson <andy.robinson@thoughtworks.com>
2011-12-13 16:50:53 +0000

    refactoring of main.js prior to extracting new object to allow testing

--- main.js
+++ main.js
@@ -2,21 +2,33 @@ examiner = require('./examiner.js');
 enigmaFactory = require('./enigma.js');
 enigma = enigmaFactory.createEnigma(6);
 
-console.log('\n\nAttempting to connect to MINISCULUS server\n--------------------------------------------\n');
+var self = exports;
+
+exports.putUrl = function(url) {
+	return url.substring(url.lastIndexOf("/"), url.indexOf("."));
+}
+
+exports.getAnswer = function(question, url) {
+	enigma.encrypt(question, function(cipher) {
+		console.log("question: "+ question);	
+		console.log("url: "+ url);
+		console.log("cipher: "+ cipher + "\n");
+
+		examiner.putAnswer(url,'{"answer":"'+ cipher +'"}', function(result) {
+			console.log("question: "+ result["question"]);
+			console.log("url: "+ result["reference-url"]);
+			console.log('\n---------------------\nEnd of test run\n');
+
+//			getAnswer(result["question"], putUrl(result["reference-url"]));
+			
+		});
+	});	
+}
+
+console.log('\n\nBeginning MINISCULUS test run\n--------------------------------------------\n');
 
 examiner.getQuestion('/start',function(result) {
-	console.log("URL: "+ result["reference-url"]);
-	console.log("question: "+ result["question"]);	
-
-	enigma.encrypt(result["question"], function(cipher) {
-		console.log("cipher: "+ cipher + "\n");
-		
-		examiner.putAnswer('/14f7ca5f6ff1a5afb9032aa5e533ad95','{"answer":"'+ cipher +'"}', function(result) {
-			console.log("URL: "+ result["reference-url"]);
-			console.log("question: "+ result["question"]);
-			console.log('\n---------------------\nEnd of response\n');
-		});
-	});
+	self.getAnswer(result["question"], self.putUrl(result["reference-url"]));
 });
 		
 
